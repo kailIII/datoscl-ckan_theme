f294a039c4bc496519eebdd7e7f4ec03ff242de1
Juan Carlos Jimenez Jamett <jcjj.system@gmail.com>
2015-10-27 14:16:15 +0000

    Agrega contenido estatico con fanstatic, imagenes y templates

---
+++ ckanext/egobcl_theme/public/js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+  var datosGob = {
+    init : function(){
+            this.slider = $('#myCarousel');
+            this.bindEvents();
+            return this;
+          },
+          initPlugins : function(){
+            // no plugins
+          },
+          bindEvents:function(){
+            var self = this;
+            if(this.slider.length){
+              this.slider.on('slid', function(e){
+                self.cambiaSlide();
+              });
+              this.slider.on('click', 'li.marker', function(e){
+                console.log('click')
+                var item = $(this).data('item');
+                self.slider.carousel(item);
+                e.preventDefault();
+              });
+            }
+          },
+          cambiaSlide:function(){
+            var self = this,
+            activeSlide = this.slider.find('div.item.active');
+            self.cambiaSlideMark(activeSlide.data('item'));
+          },
+          cambiaSlideMark : function(item){
+            var self = this,
+            markers = this.slider.find('li.marker');
+            markers.filter('.active').removeClass('active');
+            markers.filter('[data-item="'+item+'"]').addClass('active');
+          }
+        };
+
+        $(function(){
+            window.datosGob = datosGob.init();
+        });
+        
+})
